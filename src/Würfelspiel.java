4de0fd20bb6beb5b60f133cf3bb2f4ce18c805c4
Florian <florian.gugele@gmail.com>
2022-09-28 12:19:48 +0200

    Update Würfelspiel.java
    KI-Würfe und Overlay

--- src/Würfelspiel.java
+++ src/Würfelspiel.java
@@ -5,25 +5,39 @@ public class Würfelspiel {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int selectionOfUser = scanner.nextInt();
+        int selectionOfUser;
         Random random = new Random();
         int randomizer;
+        int KIrandomizer;
         int resultPlayer = 0;
         int resultKI=0;
-        boolean EndOfGame = false;
+        boolean EndOfGame=false;
+
+
 
         do {
             System.out.println("\t\tWillkommen beim Würfelspiel!\n\n1) Spiel Starten\t\t\t\t2) Spiel Beenden");
-
+            selectionOfUser = scanner.nextInt();
             switch (selectionOfUser) {
                 case 1:
                     for (int i = 1; i <= 6; i++) {
                         randomizer = random.nextInt(1, 7);
-                        System.out.println(i + " Wurf: " + randomizer);
+                        KIrandomizer=random.nextInt(1,7);
+                        System.out.println(i+"." + " Wurf: " + randomizer+"\t\t\t\t\t"+i+"."+" KI-Wurf: "+KIrandomizer);
                         resultPlayer += randomizer;
+                        resultKI+=KIrandomizer;
                     }
-                    System.out.println(resultPlayer);
+                    System.out.println("Dein Ergebnis: "+resultPlayer+"\t\t\tKI Ergebnis: "+resultKI);
 
+                    if (resultPlayer>resultKI)
+                    {
+                        System.out.println("Gewonnen!");
+                    } else if (resultPlayer==resultKI) {
+                        System.out.println("Unentschieden!");
+                    } else
+                    {
+                        System.out.println("Verloren!");
+                    }
 
                     System.out.println("Möchten Sie nochmals Spielen?\n1) Ja\t\t2) Nein");
                     int Return = scanner.nextInt();
