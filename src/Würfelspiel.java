f5bba7c93276d36987597b34a8a95ee4ce66536c
Florian <florian.gugele@gmail.com>
2022-09-28 11:52:17 +0200

    Update Würfelspiel.java
    Würfe von Spieler gemacht

--- src/Würfelspiel.java
+++ src/Würfelspiel.java
@@ -1,25 +1,34 @@
+import java.util.Random;
 import java.util.Scanner;
 
 public class Würfelspiel {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int selectionOfUser = scanner.nextInt();
+        Random random = new Random();
+        int randomizer;
+        int resultPlayer = 0;
+        int resultKI=0;
         boolean EndOfGame = false;
+
         do {
             System.out.println("\t\tWillkommen beim Würfelspiel!\n\n1) Spiel Starten\t\t\t\t2) Spiel Beenden");
-            int selectionOfUser = scanner.nextInt();
 
             switch (selectionOfUser) {
                 case 1:
-                    for (int i=1;i<=6;i++) {
-                        System.out.println(i+"Wurf: ");
+                    for (int i = 1; i <= 6; i++) {
+                        randomizer = random.nextInt(1, 7);
+                        System.out.println(i + " Wurf: " + randomizer);
+                        resultPlayer += randomizer;
                     }
+                    System.out.println(resultPlayer);
+
+
                     System.out.println("Möchten Sie nochmals Spielen?\n1) Ja\t\t2) Nein");
                     int Return = scanner.nextInt();
-                    if (Return==2)
-                    {
-                        EndOfGame=true;
+                    if (Return == 2) {
+                        EndOfGame = true;
                     }
                     break;
                 case 2:
@@ -28,6 +37,6 @@ public class Würfelspiel {
                     break;
             }
 
-        } while (EndOfGame == false);
+        } while (!EndOfGame);
     }
 }
