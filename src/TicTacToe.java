fe75d3df650d26fd76a4dff138f799d1cffbdfbf
Florian <florian.gugele@gmail.com>
2022-10-06 09:28:21 +0200

    Update TicTacToe.java

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,9 +1,25 @@
 import java.util.Scanner;
 
 public class TicTacToe {
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        
+
+        int collumn = 3;
+        int row = 3;
+
+
+        char Player1 = 'x';
+        char Player2 = 'o';
+
+
+        System.out.println("P1: Choose Position ");
+        int xy = scanner.nextInt();
+        for (int i = 0; i < row; i++) {
+            for (int e = 0; e < collumn; e++) {
+                System.out.print("|" + "");
+
+            }
+            System.out.println("|");
+        }
     }
 }
