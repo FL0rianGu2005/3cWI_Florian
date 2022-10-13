4ec74ec01ed8fd02ce3aa3f76e53a175c95d6458
Florian <florian.gugele@gmail.com>
2022-10-13 08:21:09 +0200

    Update TicTacToe.java
    Player 2 input

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,3 +1,4 @@
+import javax.crypto.spec.PSource;
 import java.util.Scanner;
 
 public class TicTacToe {
@@ -7,11 +8,15 @@ public class TicTacToe {
         char[][] playground = new char[3][3];
 
 
+        System.out.println("Player 1:");
+
         String selectionOfUser = scanner.next();
         String[] selection = selectionOfUser.split(",");
         int rowSelection = Integer.valueOf(selection[0]);
         int colSelection = Integer.valueOf(selection[1]);
         playground[rowSelection][colSelection] = 'x';
+
+
 
         for (int row = 0; row < playground.length; row++) {
             for (int col = 0; col < playground.length; col++) {
@@ -21,6 +26,9 @@ public class TicTacToe {
             System.out.println("|");
         }
 
+        System.out.println("Player 2:");
+
+        System.out.println("Test");
 
     }
 }
