91122edc37a4c811418dfd4ef65c5283f3b7a7be
Florian <florian.gugele@gmail.com>
2022-10-17 13:01:48 +0200

    Update TicTacToe.java

--- src/at/gfl/Basics/ttt/TicTacToe.java
+++ src/at/gfl/Basics/ttt/TicTacToe.java
@@ -9,48 +9,28 @@ public class TicTacToe {
         char[][] playground = new char[3][3];
         boolean isPlayer1Active = true;
 
+
+
         do {
+            String name = isPlayer1Active ? "Player 1:" : "Player 2";
+            System.out.println(name);
 
+            String selectionOfPlayer = scanner.next();
 
-            System.out.println("Player 1:");
-
-            String selectionOfPlayer1 = scanner.next();
-            String[] selection = selectionOfPlayer1.split(",");
+            String[] selection = selectionOfPlayer.split(",");
             int rowSelection = Integer.valueOf(selection[0]);
             int colSelection = Integer.valueOf(selection[1]);
+
+            while (!isFieldEmpty(playground, rowSelection, colSelection)) {
+                selectionOfPlayer = scanner.next();
+                selection = selectionOfPlayer.split(",");
+                rowSelection = Integer.valueOf(selection[0]);
+                colSelection = Integer.valueOf(selection[1]);
+            }
+
+
             if (playground[rowSelection][colSelection] == '\u0000') {
-                playground[rowSelection][colSelection] = 'x';
-            } else {
-                System.out.println("Das Feld ist schon belegt!");
-            }
-
-
-            for (int row = 0; row < playground.length; row++) {
-                for (int col = 0; col < playground.length; col++) {
-                    System.out.print("|" + playground[col][row]);
-
-                }
-                System.out.println("|");
-            }
-
-            System.out.println("Player 2:");
-
-            String selectionOfPlayer2 = scanner.next();
-
-            String[] selection2 = selectionOfPlayer2.split(",");
-            int rowSelection2 = Integer.valueOf(selection2[0]);
-            int colSelection2 = Integer.valueOf(selection2[1]);
-
-            while (!isFieldEmpty(playground, rowSelection2, colSelection2)) {
-                selectionOfPlayer2 = scanner.next();
-                selection2 = selectionOfPlayer2.split(",");
-                rowSelection2 = Integer.valueOf(selection2[0]);
-                colSelection2 = Integer.valueOf(selection2[1]);
-            }
-
-
-            if (playground[rowSelection2][colSelection2] == '\u0000') {
-                playground[rowSelection2][colSelection2] = 'o';
+                char charToPrint = isPlayer1Active?'x':'o';
             } else {
                 System.out.println("Dieses Fels ist schon belegt!");
             }
