a948d1f74259cd204007b9800042013a373c3106
Florian <florian.gugele@gmail.com>
2022-11-21 12:47:48 +0100

    Basics OO

--- src/at/gfl/ObjektOrientierung/Cars.java
+++ src/at/gfl/ObjektOrientierung/Cars.java
@@ -1,11 +1,36 @@
-
 package at.gfl.ObjektOrientierung;
 
+import at.gfl.ObjektOrientierung.Objects.CarObjects;
+
+import java.util.Scanner;
+
 public class Cars {
-    public static void main(String[] args){
+    public static void main(String[] args)
+    {
 
+        Scanner scanner = new Scanner(System.in);
 
-    Cars car1 = new Cars();
-    car1.Brand="Ferrari";
+        CarObjects c1=new CarObjects();
+        c1.brand="Ferrari";
+        c1.FuelConsumption=6;
+        c1.serialNumber="F1-75";
+        c1.FuelAmmount=30;
+        c1.maxFuel=50;
+
+        System.out.println(c1.brand);
+        System.out.println(c1.FuelConsumption);
+        System.out.println(c1.serialNumber);
+
+        c1.CarBreaking();
+        System.out.println(c1.FuelAmmount);
+        c1.CarDriving();
+        System.out.println(c1.FuelAmmount);
+
+        c1.TurboBoost();
+
+        System.out.println("How many times do you want to honk?");
+        int honker=scanner.nextInt();
+        c1.CarHonking(honker);
+
     }
 }
---
+++ src/at/gfl/ObjektOrientierung/Objects/CarObjects.java
@@ -0,0 +1,37 @@
+package at.gfl.ObjektOrientierung.Objects;
+
+public class CarObjects {
+    public int FuelConsumption;
+    public String brand;
+    public String serialNumber;
+    public int FuelAmmount;
+    public int maxFuel;
+
+    public void CarBreaking(){
+        System.out.println("Ich bremse");
+    }
+
+    public void CarDriving(){
+        this.FuelAmmount=this.FuelAmmount-this.FuelConsumption;
+        System.out.println("I´m drivin");
+    }
+    public void TurboBoost(){
+        if (this.FuelAmmount>(maxFuel*0.1)){
+            System.out.println("SuperBoostMOOOOOOOOOOOOOOODE");
+        }
+        else{
+            System.out.println("not enough Fuel for SuperBoostMOOOOOOOOOOODE");
+        }
+    }
+
+    public void CarHonking(int amountOfRepetitions){
+        if (amountOfRepetitions<=100){
+            for (int i = 0; i < amountOfRepetitions; i++) {
+                System.out.println("MEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEEP");
+            }
+        }
+        else{
+            System.out.println("Übertreib mal nicht");
+        }
+    }
+}
