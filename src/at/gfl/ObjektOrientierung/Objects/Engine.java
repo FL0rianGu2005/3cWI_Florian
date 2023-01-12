e171db496488a0f1fbd3bc93c78ff8767b14b486
Florian <florian.gugele@gmail.com>
2023-01-12 08:14:15 +0100

    engine

--- src/at/gfl/ObjektOrientierung/Objects/Engine.java
+++ src/at/gfl/ObjektOrientierung/Objects/Engine.java
@@ -1,5 +1,26 @@
 package at.gfl.ObjektOrientierung.Objects;
 
 public class Engine {
+    private enum TYPE {GAS, DIESEL}
 
+    private int horsePower;
+    private String type;
+
+    public Engine(int horsePower, TYPE type) {
+        this.horsePower = horsePower;
+        this.type = type;
+    }
+
+
+    public void drive(int amount) {
+        System.out.println("the motor is running with " + amount);
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public TYPE getType() {
+        return type;
+    }
 }
