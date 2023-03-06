8ff2177d1757146f547a9138f548c47e3922876c
Florian <florian.gugele@gmail.com>
2023-03-06 12:57:54 +0100

    Started with Lamp

---
+++ src/at/gfl/ObjektOrientierung/Lampe/Objets/Objects.java
@@ -0,0 +1,10 @@
+package at.gfl.ObjektOrientierung.Lampe.Objets;
+
+public class Objects {
+    public static class Informations{
+        private String Name;
+        private String Color;
+        private int status;
+        private boolean OnOff;
+    }
+}
