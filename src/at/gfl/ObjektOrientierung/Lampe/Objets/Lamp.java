a3258c18b02f1bd84257ccfbbf47091a84d1055b
Florian <florian.gugele@gmail.com>
2023-03-09 09:30:52 +0100

    Lamp

---
+++ src/at/gfl/ObjektOrientierung/Lampe/Objets/Lamp.java
@@ -0,0 +1,8 @@
+package at.gfl.ObjektOrientierung.Lampe.Objets;
+
+import java.util.List;
+import java.util.ArrayList;
+
+public class Lamp {
+    
+}
--- src/at/gfl/ObjektOrientierung/Lampe/Objets/Objects.java
+++ src/at/gfl/ObjektOrientierung/Lampe/Objets/Objects.java
@@ -1,10 +1,47 @@
 package at.gfl.ObjektOrientierung.Lampe.Objets;
 
 public class Objects {
-    public static class Informations{
         private String Name;
         private String Color;
         private int status;
         private boolean OnOff;
+
+    public Objects(String Name, String Color, int status, boolean OnOff) {
+        this.Name = Name;
+        this.Color = Color;
+        this.status = status;
+        this.OnOff = OnOff;
+    }
+
+    public String getName() {
+        return Name;
+    }
+
+    public void setName(String name) {
+        Name = name;
+    }
+
+    public String getColor() {
+        return Color;
+    }
+
+    public void setColor(String color) {
+        Color = color;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public boolean isOnOff() {
+        return OnOff;
+    }
+
+    public void setOnOff(boolean onOff) {
+        OnOff = onOff;
     }
 }
